10d4c2712ce9cd7b36d0feeac0265880087a32d5
Raffael Christopher <raffaelcss@hotmail.com>
2023-11-21 21:51:08 -0300

    :wrench: chore: tailwind theme modify

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    
+    extend: {
+      height: {
+        "108": "27rem",
+      },
+      width: {
+        "68": "17rem",
+      }
+    },
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [],
-}
+};
 
-export default config
+export default config;
